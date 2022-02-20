8d17d118f019e67b86fee2b148dfab3bac173412
Inakat <60667296+Inakat@users.noreply.github.com>
2022-02-20 19:45:18 +0100

    Adding Place

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,12 +1,13 @@
 import "./Search.css"
 import PriceRange from "./searchComponents/PriceRange";
 import Place from "./searchComponents/Place";
+import HouseData from "./mock.json";
 
 function Search() {
     return (
        <div className="search-body">
             <div className="searchcomponents"><PriceRange /></div>
-            <div className="searchcomponents"><Place /> </div>
+            <div className="searchcomponents"><Place placeholder="Everywhere" data={HouseData}/> </div>
       </div>
     );
   }
--- client/src/components/search/searchComponents/Place.js
+++ client/src/components/search/searchComponents/Place.js
@@ -1,18 +1,50 @@
+import React, { useState } from "react";
 import "./SearchComponents.css"
 
-function Place() {
+function Place({ placeholder, data }) {
+
+    const [filteredData, setFilteredData] = useState([]);
+
+    const handleFilter = (event) => {
+     const searchLocation = event.target.value
+     const newFilter = data.filter((value) => {
+         return value.address.locality.toLowerCase().includes(searchLocation.toLowerCase());
+     });
+     if (searchLocation === "") {
+        setFilteredData([]);
+      } else {
+      setFilteredData (newFilter);
+      }
+    };
     return (
         <div className="body">
             <div className='wrapper'>
                 <div className="header">
                         <h2> Place?</h2>
                 </div>
-                <div className="search-input"> 
+                <div className="location-input"> 
                     <div className="field">
-                        <input type="text" className="input-country" defaultValue="Everywhere">
-                        </input>
+                        <div className="locationResult"> 
+                            <input 
+                            type="text" 
+                            placeholder={placeholder}
+                            onChange={handleFilter}
+                            />
+                            <div className="dropdownIcon">
+                            </div>
+                        </div>
                     </div>
                 </div>
+                {filteredData.length != 0 && (
+                <div className="dataEnter">
+                    {filteredData.slice(0, 15).map((value, key) => {
+                        return <a className="locationItem" href={value.images[0]} target="_blank"> 
+                        <p> {value.address.locality}
+                        </p>
+                        </a>;
+                    })}
+                </div> 
+                )}
             </div>
         </div>
     );
