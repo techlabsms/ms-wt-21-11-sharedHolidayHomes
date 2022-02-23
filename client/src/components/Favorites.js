6016219d696a9275bcb345f08354215b5b73d62b
Sommi91 <paulsom91@gmail.com>
2022-02-23 21:37:40 +0100

    test connection backend;

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,23 +1,32 @@
 import React, {Component} from 'react';
 import { objects } from "./Objects";
 import './favorite.css';
+// react hooks
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-
-class Card extends Component{
-    render(){
+const Card =() => { 
+    const [houses, setHouses] = useState([]);
+    useEffect(() => {
+    const fetchHouses = async () => {
+        const result = await axios ({method:"get", url:"http://localhost:5000/houses"})
+        setHouses(result.data)
+    }
+    fetchHouses()
+    })
         return (
             <div>
                 <div class="tc ma2 pa3">
                     <hr color='#a3e8d6' />
                     <h1 className="light-green pa3">Unsere Empfehlungen für Dich!</h1>
 
-                    {objects.map((item) => {
+                    {houses.map((item) => {
                         return (
                             <div className="gallery tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5">
-                                <a href=''><img src={item.picture} alt={item.alt} height="180px" witdh="180px"/></a>
+                                <a href=''><img src={item.image[0]} height="180px" witdh="180px"/></a>
                                 <div className="ma1 pa2">
                                     <h2>{item.name}</h2>
-                                    <h4>{item.location}</h4>
+                                    <h4>{item.address.locality}</h4>
                                 </div>
                             </div>   
                             )
@@ -30,7 +39,7 @@ class Card extends Component{
                 </div>
             </div>
         )
-    }
+    
 }
         
 
