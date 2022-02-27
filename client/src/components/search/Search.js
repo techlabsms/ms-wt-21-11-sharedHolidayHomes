346169f33aaba35ad07b013f17d1ed2e3518f024
Inakat <60667296+Inakat@users.noreply.github.com>
2022-02-27 18:23:25 +0100

    translation header

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -10,8 +10,8 @@ function Search() {
       <div>
      
         <div className="search-body">
-          <h1 className="f2 fw9 white tc ma2"> <strong>Der smarte Weg in dein Shared-Holiday Home</strong></h1>
-          <h3 className="f3 white tc ma4">Finde Dein neues Zuhause an den schönsten Orten der Welt!</h3>
+          <h1 className="f2 fw9 white tc ma2"> <strong>The smart way to your Shared Holiday Home</strong></h1>
+          <h3 className="f3 white tc ma4">Find your new home in the most beautiful places around the world!</h3>
         
             <div className="searchcomponents"><SearchComponents placeholder="Everywhere" data={HouseData}/></div>
         </div>
