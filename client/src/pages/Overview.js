759e4528ee2d07af37dbbf9bf1ab60e0f3aef722
jandennisw <94896829+jandennisw@users.noreply.github.com>
2022-02-28 20:00:03 +0100

    Update Overview.js

--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Overview(){
     return (
         <div>
-            <h1>On this page the searched objects will be listed</h1>
+            <h1>Just wait a week. Shortly you can see your search results</h1>
             <Link to="/"><button className="grow" type='submit'>Back to Homepage</button></Link>
         </div>
 
