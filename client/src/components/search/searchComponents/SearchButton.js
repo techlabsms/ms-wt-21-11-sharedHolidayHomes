aae4be17d787fa60f7cff33e24052ae0bc911a35
jandennisw <94896829+jandennisw@users.noreply.github.com>
2022-03-01 10:48:00 +0100

    Update SearchButton.js

--- client/src/components/search/searchComponents/SearchButton.js
+++ client/src/components/search/searchComponents/SearchButton.js
@@ -5,7 +5,7 @@ function SearchButton() {
     return (
         <div className="body">
            <div> 
-                <Link to="Overview"><button className="searchButton grow" type='submit'>Search</button></Link>
+                <Link to="Overview"><button className="searchButton grow" type='submit'>Search now</button></Link>
             </div>
         </div>
     );
