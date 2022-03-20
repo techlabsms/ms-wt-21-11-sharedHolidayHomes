09c2f0c44c51191826a24b19b1d58a8a2ae6dc63
juliascheidt <95585503+juliascheidt@users.noreply.github.com>
2022-03-20 20:52:20 +0100

    Fix typo (google-map-react vs google-maps-react) and force install google-maps-react

--- client/src/pages/parts/map.js
+++ client/src/pages/parts/map.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import './map.css';
-import { useParams } from "react-router-dom";
-
+import { useParams } from 'react-router-dom';
 
 export class MapContainer extends Component {
-
-  render() {  
+  render() {
     return (
-        <div className='map'>
-            <Map google={this.props.google} zoom={14} initialCenter = {{lat: 48.16725, lng: 11.60236}}>
-        
-                <Marker onClick={this.onMarkerClick}
-                        name={'Current location'} />
-                      
-        
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    {/* <div>
+      <div className='map'>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          initialCenter={{ lat: 48.16725, lng: 11.60236 }}
+        >
+          <Marker onClick={this.onMarkerClick} name={'Current location'} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            {/* <div>
                     <h1>{this.state.selectedPlace.name}</h1>
                     </div> */}
-                </InfoWindow>
-            </Map>
-        </div>
+          </InfoWindow>
+        </Map>
+      </div>
     );
   }
 }
 
- 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyAoSqBI70Dp7qrsmP1ypCVboTY33gi8JWg")
-})(MapContainer)
+  apiKey: 'AIzaSyAoSqBI70Dp7qrsmP1ypCVboTY33gi8JWg',
+})(MapContainer);
